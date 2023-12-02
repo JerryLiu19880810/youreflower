f426ce39227ef7b844ccabafb3dec91226730b78
J7 <j7.dev.gg@outlook.com>
2023-12-03 07:58:34 +0800

    fix: add exception to product gallery

--- wp-content/themes/customstrap/page-home.php
+++ wp-content/themes/customstrap/page-home.php
@@ -54,11 +54,11 @@ if (function_exists('rwmb_meta')) {
 <div class="swiper-container" style="aspect-ratio: 1860/750;">
 	<div class="swiper-wrapper">
 		<?php
-		foreach ($slider as $slide) :
+		foreach ($slider as $slide):
 			$img_url = wp_get_attachment_url($slide['slide_image']);
 			$link_html = empty($slide['slide_link']) ? '' : '<a href="' . $slide['slide_link'] . '">';
 			$link_html_end = empty($slide['slide_link']) ? '' : '</a>';
-		?>
+			?>
 			<div class="swiper-slide">
 				<?= $link_html ?>
 				<div class="image" style="background-image:url(<?= $img_url ?>);"></div>
@@ -86,14 +86,14 @@ if (function_exists('rwmb_meta')) {
 <section class="py-1">
 	<div class="container-full">
 		<div class="row gutter-1">
-			<?php foreach ($_2col as $col) :
+			<?php foreach ($_2col as $col):
 				$left_image_url = wp_get_attachment_url($col['left_image']);
 				$left_link_html = empty($col['left_link']) ? '' : '<a href="' . $col['left_link'] . '">';
 				$left_link_html_end = empty($col['left_link']) ? '' : '</a>';
 				$right_image_url = wp_get_attachment_url($col['right_image']);
 				$right_link_html = empty($col['right_link']) ? '' : '<a href="' . $col['right_link'] . '">';
 				$right_link_html_end = empty($col['right_link']) ? '' : '</a>';
-			?>
+				?>
 
 				<div class="col-md-6">
 					<div class="card card-tile">
@@ -130,10 +130,12 @@ if (function_exists('rwmb_meta')) {
 						<span class="eyebrow text-muted">Hot Products</span>
 						<h2>Top Sellers</h2>
 						<div class="nav nav-tabs flex-lg-column mt-md-3 lavalamp">
-							<?php foreach ($product_cats as $key => $product_cat) :
+							<?php foreach ($product_cats as $key => $product_cat):
 								$active = $key == 0 ? 'active' : '';
-							?>
-								<a class="nav-item nav-link <?= $active ?>" data-filter="<?= $key + 1 ?>"><?= $product_cat->name ?></a>
+								?>
+								<a class="nav-item nav-link <?= $active ?>" data-filter="<?= $key + 1 ?>">
+									<?= $product_cat->name ?>
+								</a>
 							<?php endforeach; ?>
 						</div>
 					</div>
@@ -142,7 +144,7 @@ if (function_exists('rwmb_meta')) {
 			<div class="col-lg-9">
 				<div class="row gutter-2 filtr-container imagesloaded">
 
-					<?php foreach ($product_cats as $key => $product_cat) :
+					<?php foreach ($product_cats as $key => $product_cat):
 						$active = $key == 0 ? 'active' : '';
 						$args = array(
 							'post_type' => 'product',
@@ -156,17 +158,19 @@ if (function_exists('rwmb_meta')) {
 							),
 						);
 						$products = new WP_Query($args);
-					?>
+						?>
 
 						<div class="col-12 filtr-item" data-category="<?= $key + 1 ?>" data-sort="value">
-							<div class="owl-carousel owl-carousel--mask visible" data-items="[2,2,2,1]" data-loop="true" data-margin="10" data-nav="true">
+							<div class="owl-carousel owl-carousel--mask visible" data-items="[2,2,2,1]"
+								data-loop="true" data-margin="10" data-nav="true">
 								<?php
-								while ($products->have_posts()) : $products->the_post();
+								while ($products->have_posts()):
+									$products->the_post();
 									global $product;
 									$attachment_ids = $product->get_gallery_image_ids();
 									$product_img_1 = empty($attachment_ids[0]) ? '' : '<img src="' . wp_get_attachment_url($attachment_ids[0]) . '">';
 									$product_img_2 = empty($attachment_ids[1]) ? '' : '<img src="' . wp_get_attachment_url($attachment_ids[1]) . '">';
-								?>
+									?>
 									<div class="card card-product">
 										<figure class="card-image">
 											<a href="#!" class="action"><i class="icon-heart"></i></a>
@@ -176,8 +180,12 @@ if (function_exists('rwmb_meta')) {
 											</a>
 										</figure>
 										<a href="" class="card-body">
-											<h3 class="card-title"><?= get_the_title(); ?></h3>
-											<span class="price"><?php echo $product->get_price_html(); ?></span>
+											<h3 class="card-title">
+												<?= get_the_title(); ?>
+											</h3>
+											<span class="price">
+												<?php echo $product->get_price_html(); ?>
+											</span>
 										</a>
 									</div>
 
@@ -210,26 +218,36 @@ if (function_exists('rwmb_meta')) {
 		<div class="row gutter-1">
 			<?php
 			$_pf = new WC_Product_Factory();
-			foreach ($collect_products as $collect_product_id) :
+			foreach ($collect_products as $collect_product_id):
 				//$product = wc_get_product($collect_product_id);
-				$product = wc_get_product($collect_product_id);
-				$attachment_ids = $product->get_gallery_image_ids();
-				$product_image = empty($attachment_ids[0]) ? '' : wp_get_attachment_url($attachment_ids[0]);
-				//$product_image = wp_get_attachment_url(get_post_thumbnail_id($collect_product_id));
-				$product_title = get_the_title($collect_product_id);
-				$product_url = get_the_permalink($collect_product_id)
-			?>
+				if ($collect_product_id):
 
-				<div class="col-6 col-lg-3">
-					<a href="<?= $product_url; ?>">
-						<figure class="category category--alt">
-							<div class="equal"><span class="image" style="background-image: url(<?= $product_image; ?>)"></span></div>
-							<figcaption><?= $product_title; ?></figcaption>
-						</figure>
-					</a>
-				</div>
+					$product = wc_get_product($collect_product_id) ?? null;
+					if ($product):
+						$attachment_ids = $product->get_gallery_image_ids();
+						$product_image = empty($attachment_ids[0]) ? '' : wp_get_attachment_url($attachment_ids[0]);
+						//$product_image = wp_get_attachment_url(get_post_thumbnail_id($collect_product_id));
+						$product_title = get_the_title($collect_product_id);
+						$product_url = get_the_permalink($collect_product_id)
+							?>
 
-			<?php endforeach; ?>
+						<div class="col-6 col-lg-3">
+							<a href="<?= $product_url; ?>">
+								<figure class="category category--alt">
+									<div class="equal"><span class="image"
+											style="background-image: url(<?= $product_image; ?>)"></span></div>
+									<figcaption>
+										<?= $product_title; ?>
+									</figcaption>
+								</figure>
+							</a>
+						</div>
+
+						<?php
+					endif;
+
+				endif;
+			endforeach; ?>
 		</div>
 	</div>
 </section>
@@ -237,11 +255,11 @@ if (function_exists('rwmb_meta')) {
 
 <section class="pt-0">
 	<div class="container">
-		<?php foreach ($banners as $banner) :
+		<?php foreach ($banners as $banner):
 			$img_url = wp_get_attachment_url($banner['banner_image']);
 			$link_html = empty($banner['banner_link']) ? '' : '<a href="' . $banner['banner_link'] . '">';
 			$link_html_end = empty($banner['banner_link']) ? '' : '</a>';
-		?>
+			?>
 			<?= $link_html; ?>
 			<img class="w-100" src="<?= $img_url; ?>" alt="">
 			<?= $link_html_end; ?>
